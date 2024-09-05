f3ce5f2f21cfe3be178b3a8f5cbe129d0e69a1f8
Yehor Polkovnychenko <eoxa77@gmail.com>
2024-09-05 18:02:20 +0200

    Updated client app for CI

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -15,9 +15,9 @@ export default observer(function HomePage() {
                 </Header>
                 {userStore.isLoggedIn ? (
                     <>
-                        <Header as='h2' inverted content='Welcome to Reactivities' />
+                        <Header as='h2' inverted content='Welcome to Reactivities!' />
                         <Button as={Link} to='/activities' size="huge" inverted>
-                            Go to Activities
+                            Go to Activities!!!
                         </Button>
                     </>
                 ) : (
